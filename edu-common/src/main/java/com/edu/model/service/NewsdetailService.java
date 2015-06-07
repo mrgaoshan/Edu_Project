8dde302cebdc95b58827d7980fff0608ae6c47cc
gaoshan <mrgaoshan@foxmail.com>
2015-06-07 16:13:32 +0800

    add view details

--- edu-common/src/main/java/com/edu/model/service/NewsdetailService.java
+++ edu-common/src/main/java/com/edu/model/service/NewsdetailService.java
@@ -1,6 +1,7 @@
 package com.edu.model.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.edu.base.service.BaseAppService;
 import com.edu.model.bean.Newsdetail;
@@ -11,17 +12,45 @@ import java.util.List;
 import java.util.Map;
 
 @Service("newsdetailService")
-public class NewsdetailService extends BaseAppService<NewsdetailMapper>{
+public class NewsdetailService extends BaseAppService<NewsdetailMapper> {
 
 	private static final long serialVersionUID = -478225924502967138L;
-	
-	
-	public List<Newsdetail> findBySchoolAndCate(Integer schoolId,String cate) {
-		
+
+	public List<Newsdetail> findBySchoolAndCate(Integer schoolId, String cate) {
+
 		Map map = new HashMap();
 		map.put("schoolId", schoolId);
 		map.put("cate", cate);
 		return mapper.findBySchoolAndCate(map);
+	}
+
+	public Newsdetail updateAndView(Integer id) {
+
+		Newsdetail newsdetail = (Newsdetail) selectByPrimaryKey(id);
+
+		updateReadTime(newsdetail);
+
+		return newsdetail;
+	}
+
+	public void updateReadTime(Newsdetail newsdetail) {
+
+		if (null != newsdetail) {
+			Integer readtimes;
+
+			readtimes = newsdetail.getReadtimes();
+
+			if (null == readtimes) {
+				readtimes = 0;
+			}
+
+			readtimes++;
+
+			newsdetail.setReadtimes(readtimes);
+
+			updateByPrimaryKeySelective(newsdetail);
+		}
+
 	}
 
 }
--- edu-web/src/main/java/com/edu/web/action/HomeAction.java
+++ edu-web/src/main/java/com/edu/web/action/HomeAction.java
@@ -19,26 +19,34 @@ import com.edu.model.service.NewsdetailService;
  *
  */
 @Controller
+@RequestMapping(value = "home")
 public class HomeAction {
-	
-    @Autowired
-    private NewsdetailService newsdetailService;
-    
-    @RequestMapping(value="home")
-    public String homePage(Model model){
-    	List<Newsdetail> gkxxList	= newsdetailService.findBySchoolAndCate(0, Constant.NewsCategory.GKXX);
-    	List<Newsdetail> zsjjList	= newsdetailService.findBySchoolAndCate(0, Constant.NewsCategory.ZSJJ);
-    	List<Newsdetail> xxxxList	= newsdetailService.findBySchoolAndCate(0, Constant.NewsCategory.XXXX);
-    	List<Newsdetail> zkxxList	= newsdetailService.findBySchoolAndCate(0,Constant.NewsCategory.ZKXX);
-    	
-    	model.addAttribute("gkxxList", gkxxList);
-    	model.addAttribute("zsjjList", zsjjList);
-    	model.addAttribute("xxxxList", xxxxList);
-    	model.addAttribute("zkxxList", zkxxList);
-    	
-    	return "home";
-    	
-    	
-    }
+
+	@Autowired
+	private NewsdetailService newsdetailService;
+
+	@RequestMapping(value = "home")
+	public String homePage(Model model) {
+		List<Newsdetail> gkxxList = newsdetailService.findBySchoolAndCate(0, Constant.NewsCategory.GKXX);
+		List<Newsdetail> zsjjList = newsdetailService.findBySchoolAndCate(0, Constant.NewsCategory.ZSJJ);
+		List<Newsdetail> xxxxList = newsdetailService.findBySchoolAndCate(0, Constant.NewsCategory.XXXX);
+		List<Newsdetail> zkxxList = newsdetailService.findBySchoolAndCate(0, Constant.NewsCategory.ZKXX);
+
+		model.addAttribute("gkxxList", gkxxList);
+		model.addAttribute("zsjjList", zsjjList);
+		model.addAttribute("xxxxList", xxxxList);
+		model.addAttribute("zkxxList", zkxxList);
+
+		return "home";
+
+	}
+
+	@RequestMapping(value = "viewNews")
+	public String viewNews(Model model, Integer id) {
+
+		newsdetailService.updateAndView(id);
+		return "viewNews";
+
+	}
 
 }
